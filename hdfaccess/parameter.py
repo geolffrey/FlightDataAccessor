2e36ede10ffcf7809ef41bd965cec4d7f6444f30
Chris Jesse <chris.jesse@flightdataservices.com>
2011-12-30 17:37:53 +0000

    Removed circular dependency on analyis

--- hdfaccess/parameter.py
+++ hdfaccess/parameter.py
@@ -5,13 +5,8 @@
 Parameter container class.
 '''
 
-import numpy as np
-
-from analysis.library import align, value_at_time # WARNING: Circular dependency on repository
-
-
 class Parameter(object):
-    def __init__(self, name, array=np.ma.masked_array([]), frequency=1, offset=0):
+    def __init__(self, name, array=[], frequency=1, offset=0):
         '''
         :param name: Parameter name
         :type name: String
@@ -31,24 +26,4 @@ class Parameter(object):
     def __repr__(self):
         return "%s %sHz %.2fsecs" % (self.name, self.frequency, self.offset)
     
-    def get_aligned(self, param):
-        aligned_array = align(self, param)
-        return self.__class__(self.name, array=aligned_array,
-                              frequency=param.frequency, offset=param.offset)
-    
-    def at(self, secs):
-        """
-        Interpolates to retrieve the most accurate value.
-        
-        :param secs: time delta from start of data in seconds
-        :type secs: float or timedelta
-        """
-        try:
-            # get seconds from timedelta
-            secs = float(secs.total_seconds)
-        except AttributeError:
-            # secs is a float
-            secs = float(secs)
-        return value_at_time(self.array, self.frequency, self.offset, secs)
-        
-P = Parameter # shorthand
+
