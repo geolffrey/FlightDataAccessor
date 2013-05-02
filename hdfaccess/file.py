f745514ec695c6b20bb3134730c57dc7ff8f130b
Glen Kirkup <glen.kirkup@flightdataservices.com>
2013-05-02 12:22:41 +0100

    Always write parameter invalid attribute to fix the Analyzer.

--- hdfaccess/file.py
+++ hdfaccess/file.py
@@ -623,8 +623,8 @@ class hdf_file(object):    # rare case of lower case?!
         # HDF file as an attribute.
         if hasattr(param, 'arinc_429') and param.arinc_429 is not None:
             param_group.attrs['arinc_429'] = param.arinc_429
-        if hasattr(param, 'invalid') and param.invalid is not None:
-            param_group.attrs['invalid'] = param.invalid
+        param_group.attrs['invalid'] = \
+            bool(hasattr(param, 'invalid') and param.invalid)
         if hasattr(param, 'invalidity_reason') and param.invalidity_reason is not None:
             param_group.attrs['invalidity_reason'] = param.invalidity_reason
         if hasattr(param, 'units') and param.units is not None:
