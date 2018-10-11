6c2ab1cfd35084cc383b85c45c66a152b4a2da26
Marek Petlicki <marek.petlicki@gmail.com>
2018-10-11 11:19:00 +0100

    Use different module for deprecation warnings
    Useful feature of modifying the docstrings for generated documentation
    and control of deprecation and removal of features if version is
    declared.

--- flightdataaccessor/formats/legacy.py
+++ flightdataaccessor/formats/legacy.py
@@ -1,4 +1,4 @@
-from deprecated import deprecated
+from deprecation import deprecated
 
 
 REMOVE_GLOBAL_ATTRIBUTES = [
@@ -16,49 +16,61 @@ class Compatibility(object):
     """Support for legacy properties and methods"""
 
     # Legacy functions for handling pickled global attributes:
-    @deprecated(reason='Use standard attribute read instead')
+    @deprecated(details='Use standard attribute read instead')
     def get_attr(self, name, default=None):
         return getattr(self, name, default)
 
-    @deprecated(reason='Use standard attribute assignment instead')
+    @deprecated(details='Use standard attribute assignment instead')
     def set_attr(self, name, value):
         return setattr(self, name, value)
 
     # Legacy functions for retrieving/storing/deleting parameter data:
-    @deprecated(reason="Use `get_parameter()` instead")
+    @deprecated(details="Use `get_parameter()` instead")
     def get_param(self, *args, **kwargs):
         return self.get_parameter(*args, **kwargs)
 
-    @deprecated(reason="Use `set_parameter()` instead")
+    @deprecated(details='Use `get_parameter_limits()` instead')
+    def get_param_limits(self, *args, **kwargs):
+        return self.get_parameter_limits(*args, **kwargs)
+
+    @deprecated(details="Use `set_parameter()` instead")
     def set_param(self, *args, **kwargs):
         return self.set_parameter(*args, **kwargs)
 
-    @deprecated(reason='Use `get_parameters()` instead')
-    def get_params(self, *args, **kwargs):
-        return self.get_parameters(*args, **kwargs)
+    @deprecated(details='Use `set_parameter_limits()` instead')
+    def set_param_limits(self, *args, **kwargs):
+        return self.set_parameter_limits(*args, **kwargs)
 
-    @deprecated(reason="Use `delete_parameters()` instead")
+    @deprecated(details='Use `set_parameter_invalid()` instead')
+    def set_invalid(self, *args, **kwargs):
+        return self.set_parameter_invalid(*args, **kwargs)
+
+    @deprecated(details='Use `get_parameters()` instead')
+    def get_params(self, param_names=None, **kwargs):
+        return self.get_parameters(names=param_names, **kwargs)
+
+    @deprecated(details="Use `delete_parameters()` instead")
     def delete_params(self, *args, **kwargs):
         return self.delete_parameters(*args, **kwargs)
 
     # Legacy functions for looking up different groups of parameter names:
-    @deprecated(reason="Use `keys()` instead")
+    @deprecated(details="Use `keys()` instead")
     def get_param_list(self):
         return self.keys()
 
-    @deprecated(reason="Use `keys(valid_only=True)` instead")
+    @deprecated(details="Use `keys(valid_only=True)` instead")
     def valid_param_names(self):
         return self.keys(valid_only=True)
 
-    @deprecated(reason="Use `keys(valid_only=True, subset='source')` instead")
+    @deprecated(details="Use `keys(valid_only=True, subset='source')` instead")
     def valid_lfl_param_names(self):
         return self.keys(valid_only=True, subset='source')
 
-    @deprecated(reason="Use `keys(subset='source')` instead")
+    @deprecated(details="Use `keys(subset='source')` instead")
     def lfl_keys(self):
         return self.keys(subset='source')
 
-    @deprecated(reason="Use `keys(subset='derived')` instead")
+    @deprecated(details="Use `keys(subset='derived')` instead")
     def derived_keys(self):
         return self.keys(subset='derived')
 
@@ -67,7 +79,7 @@ class Compatibility(object):
     # - version
 
     @property
-    @deprecated(reason='Use `file` instead')
+    @deprecated(details='Use `file` instead')
     def hdf(self):
         return self.file  # FIXME: What about accessing ['series']?
 
