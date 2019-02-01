50e6196f59c44e95c7f27b6ceb528e41cc78a285
Marek Petlicki <marek.petlicki@flightdataservices.com>
2019-02-01 14:45:29 +0000

    PEP 390 compatible metadata definition
    We need to avoid importing the package in setup.py (for build) because
    we have imports on package level and we trigger missing imports. It's
    better to use setup.cfg instead.

--- flightdataaccessor/__init__.py
+++ flightdataaccessor/__init__.py
@@ -4,30 +4,6 @@
 # http://www.flightdataservices.com
 # See the file "LICENSE" for the full license governing this code.
 
-__packagename__ = 'HDFAccess'
-__version__ = '0.1.4'
-__author__ = 'Flight Data Services Ltd'
-__author_email__ = 'developers@flightdataservices.com'
-__maintainer__ = 'Flight Data Services Ltd'
-__maintainer_email__ = 'developers@flightdataservices.com'
-__url__ = 'http://www.flightdatacommunity.com/'
-__description__ = 'An interface for HDF files containing flight data.'
-__download_url__ = ''
-__classifiers__ = [
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Operating System :: OS Independent',
-    'Topic :: Software Development',
-]
-__platforms__ = ['OS Independent']
-__license__ = 'Open Software License (OSL-3.0)'
-__keywords__ = ['hdf', 'numpy', 'flight', 'data']
-
-
 from .datatypes.array import MappedArray  # noqa: F401
 from .datatypes.parameter import Parameter  # noqa: F401
 from .formats.base import FlightDataFormat  # noqa: F401
--- setup.py
+++ setup.py
@@ -4,29 +4,12 @@
 # http://www.flightdataservices.com
 # See the file "LICENSE" for the full license governing this code.
 
-import os
-
-import flightdataaccessor as pkg
 from setuptools import setup, find_packages
 from requirements import RequirementsParser
 requirements = RequirementsParser()
 
 setup(
-    name=pkg.__packagename__,
-    version=pkg.__version__,
-    author=pkg.__author__,
-    author_email=pkg.__author_email__,
-    maintainer=pkg.__maintainer__,
-    maintainer_email=pkg.__maintainer_email__,
-    url=pkg.__url__,
-    description=pkg.__description__,
-    long_description=open('README.rst').read() + open('CHANGES').read() +
-    open('TODO').read() + open('AUTHORS').read(),
-    download_url=pkg.__download_url__,
-    classifiers=pkg.__classifiers__,
-    platforms=pkg.__platforms__,
-    license=pkg.__license__,
-    keywords=pkg.__keywords__,
+    long_description=open('README.rst').read() + open('CHANGES').read() + open('TODO').read() + open('AUTHORS').read(),
     packages=find_packages(exclude=('tests',)),
     include_package_data=True,
     zip_safe=False,
