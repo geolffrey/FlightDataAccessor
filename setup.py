7bebbeb105c6ee826779ff93b1843a9c85bc423c
Glen Kirkup <glen.kirkup@flightdataservices.com>
2019-09-13 10:55:44 +0100

    updates setup.py to match master

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (c) Flight Data Services Ltd
-# http://www.flightdataservices.com
-# See the file "LICENSE" for the full license governing this code.
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-from requirements import RequirementsParser
-
-requirements = RequirementsParser()
-
-setup(
-    long_description=open('README.rst').read() + open('CHANGES').read() + open('TODO').read() + open('AUTHORS').read(),
-    packages=find_packages(exclude=('tests',)),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=requirements.install_requires,
-    setup_requires=requirements.setup_requires,
-    tests_require=requirements.tests_require,
-    extras_require=requirements.extras_require,
-    dependency_links=requirements.dependency_links,
-    test_suite='nose.collector',
-)
-
-################################################################################
-# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
+setup(use_scm_version={'fallback_version': '19.0.0'})
